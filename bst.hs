1d58e180844786d44983854ec162ca6f74461f51
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-07-16 15:57:52 -0300

    delete?

--- bst.hs
+++ bst.hs
@@ -1,25 +1,42 @@
-module BST where
+data BinarySearchTree a = NIL | Node (BinarySearchTree a) Int (BinarySearchTree a) deriving (Eq,Show)
 
-data BinarySearchTree a = NIL | Node a (BinarySearchTree a) (BinarySearchTree a) deriving (Eq,Show)
-
-{-verifica se a arvore está vazia-}
 empty NIL = True
 empty _ = False
 
-{-verifica o tamanho da arvore-}
 treeSize NIL = 0
-treeSize (Node a left right) = 1 + treeSize left + treeSize right
-
-{-arvore vazia, adiciona como raiz e só-}
-insert NIL node = Node node NIL NIL
-{--arvore nao vazia--}
-insert (Node current left right) node | current == node = Node current left right
-									  | current < node = Node current left (insert right node)
-									  | current > node = Node current (insert left node) right
+treeSize (Node left a right) = 1 + treeSize left + treeSize right
 
 
+insert NIL node = Node NIL node NIL
+insert (Node  left current right) node 
+ | current == node = Node left current right
+ | current < node = Node left current (insert right node)
+ | current > node = Node (insert left node) current right
+
+delete NIL _ = NIL
+delete (Node t1 v t2) x  
+ | x == v = deleteX (Node t1 v t2)
+ | x  < v = Node (delete t2 x) v t2
+ | x  > v = Node t1 v (delete t2 x)
+
+deleteX (Node NIL v t2) = t2
+deleteX (Node t1 v NIL) = t1
+deleteX (Node t1 v t2) = (Node t1 v2 t2) --(delete t2 v2))
+ where 
+  v2 = leftistElement t2
 
 
+leftistElement (Node NIL v _) = v
+leftistElement (Node t1 _ _) = leftistElement t1
+order NIL = []
+order (Node left a right) =  order left ++ [a] ++ order right
 
 
+main::IO()
+main = do
+ let a = insert NIL 5
+ let a2 = insert a 3
+ let a3 = insert a 8
+ let b = order a3
 
+ print b
