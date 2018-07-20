6e2232f61047bde3a8deb5d60d1a24448aa76f45
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2018-07-20 15:42:23 -0300

    terminando BST

--- bst.hs
+++ bst.hs
@@ -4,13 +4,15 @@ empty NIL = True
 empty _ = False
 
 treeSize NIL = 0
-treeSize (Node left a right) = 1 + treeSize left + treeSize right
+treeSize (Node left current right) = 1 + treeSize left + treeSize right
 
-contains NIL _ = False
-contains (Node left current right) node
+--operações básicas 
+
+search NIL _ = False
+search (Node left current right) node
  | current == node = True
- | node < current = contains left node
- | node > current = contains right node 
+ | node < current = search left node
+ | node > current = search right node 
 
 insert NIL node = Node NIL node NIL
 insert (Node  left current right) node 
@@ -30,6 +32,9 @@ deleteX (Node t1 v t2) = (Node t1 v2 t2) --(delete t2 v2))
  where 
   v2 = leftistElement t2
 
+height NIL = 0
+height (Node left current right) = 1 + max (height left) (height right)
+
 leftistElement (Node NIL v _) = v
 leftistElement (Node t1 _ _) = leftistElement t1
 
@@ -44,10 +49,30 @@ preOrder (Node left current right) = [current] ++ preOrder left ++ preOrder righ
 posOrder NIL = []
 posOrder (Node left current right) = posOrder left ++ posOrder right ++ [current]
 
+--Mínimo e máximo
+
+minimo NIL = 0
+minimo (Node NIL current NIL) = current
+minimo (Node left current right) = minimo (left)
+
+maximo NIL  = 0
+maximo (Node NIL current NIL) = current
+maximo (Node left current right) = maximo (right)
+
+--Sucessor e Predecessor
+
+predecessor NIL = NIL
+
+sucessor NIL = NIL
+
 main::IO()
 main = do 
  let a = insert NIL 5
  let a2 = insert a 3
  let a3 = insert a2 8
- let b = contains a3 7
+ let a4 = insert a3 10
+ let a6 = insert a4 110
+ let b = maximo a6
+
  print b
+ 
