e132079272a608ec9a2e718fa1db61f9a1df8148
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-07-21 19:22:19 -0300

    rebalances feitos

--- avl.hs
+++ avl.hs
@@ -1,10 +1,14 @@
-import BST
+module AVL where 
+import qualified BST as BST
+
 
 --rotacoes 
+balanceFactor left right = (BST.height left) - (BST.height right)
 
-balanceFactor NIL = 0
-balanceFactor (Node NIL current NIL) = 0
-balanceFactor (Node left current right) = height left - height right
 
-	
+balanceLL (BST.Node (BST.Node leftLeftSon leftSon leftRightSon) node rightSon) = (BST.Node leftLeftSon leftSon (BST.Node leftRightSon node rightSon))
+balanceLR (BST.Node (BST.Node leftLeftSon leftSon (BST.Node lrlSon leftRightSon lrrSon)) node rightSon) = (BST.Node (BST.Node leftLeftSon leftSon lrlSon) leftRightSon (BST.Node lrrSon node rightSon))
+balanceRL (BST.Node leftSon node (BST.Node (BST.Node rllSon rightLeftSon rlrSon) rightSon rightRightSon)) = (BST.Node (BST.Node leftSon node rllSon) rightLeftSon (BST.Node rlrSon rightSon rightRightSon)) 
+balanceRR (BST.Node leftSon node (BST.Node rightLeftSon rightSon rightRightSon)) = (BST.Node (BST.Node leftSon node rightLeftSon) rightSon rightRightSon)
+
 
